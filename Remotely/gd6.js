c06deb97bd15196dc09ed491f8f0dc0161a01513
wangsvven <134424041+wangsvven@users.noreply.github.com>
2023-08-31 07:19:40 +0800

    Update gd6.js

--- Remotely/gd6.js
+++ Remotely/gd6.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/goodnotes6.js
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/goodnotes6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/wangsvven/QuantumltX/main/Remotely/gd6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/wangsvven/QuantumltX/main/Remotely/gd6.js
 
 [mitm]
 hostname = isi.csan.goodnotes.com
