0265e4220f5d074b9a58f917e1bcb49d96c8e6c7
wangsvven <134424041+wangsvven@users.noreply.github.com>
2023-10-09 22:16:39 +0800

    Update MJai.js

--- Remotely/MJai.js
+++ Remotely/MJai.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/MJ.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/wangsvven/QuantumltX/main/Remotely/MJai.js
 
 [mitm]
 hostname = buy.itunes.apple.com
